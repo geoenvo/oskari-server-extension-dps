9ca910418d7db3697aba4a745bdc00754cd6e8d1
Jani Levonen <jani.levonen@sitowise.com>
2020-06-29 08:38:16 +0300

    Add CKANDataParser class

---
+++ server-extension/src/main/java/wbidp/oskari/parser/CKANDataParser.java
@@ -0,0 +1,35 @@
+package wbidp.oskari.parser;
+
+import java.util.ArrayList;
+
+import fi.nls.oskari.domain.User;
+import fi.nls.oskari.log.LogFactory;
+import fi.nls.oskari.log.Logger;
+
+public class CKANDataParser {
+
+    private static final Logger LOG = LogFactory.getLogger(CKANDataParser.class);
+
+    /**
+     * Parses JSON user data from CKAN to Oskari Users.
+     * 
+     * @param JSONFromCKAN user data from CKAN as JSON.
+     * @return an ArrayList of Oskari User objects.
+     */
+    public ArrayList<User> parseJSONToUsers(String JSONFromCKAN) {
+        ArrayList<User> users = new ArrayList<>();
+        LOG.info("Parsing CKAN JSON data.");
+
+        // TODO: Parse CKAN user data JSON to Oskari Users here
+        // and add each created user to the ArrayList.
+        //
+        // Oskari user can be created like this:
+        // User user = new User();
+        // user.setScreenname("TT");
+        // user.setFirstname("Testi");
+        // user.setLastname("Teppo");
+        // user.setEmail("");
+
+        return users;
+    }
+}
