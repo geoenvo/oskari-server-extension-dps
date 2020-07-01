1b5dcfd74a062f2bff3234e9ca5013836ad2ecf6
Jani Levonen <jani.levonen@sitowise.com>
2020-07-01 16:28:30 +0300

    Fix some imports and add comments

--- server-extension/src/main/java/flyway/wbidp/V1_0_2__add_content_editor.java
+++ server-extension/src/main/java/flyway/wbidp/V1_0_2__add_content_editor.java
@@ -1,11 +1,15 @@
 package flyway.wbidp;
 
 import fi.nls.oskari.util.FlywayHelper;
+import org.flywaydb.core.api.migration.jdbc.JdbcMigration;
 
 import java.sql.Connection;
 import java.util.List;
 
-public class V1_0_2__add_content_editor {
+/**
+ * Adds content-editor bundle to default and user views.
+ */
+public class V1_0_2__add_content_editor implements JdbcMigration {
     private static final String BUNDLE_ID = "content-editor";
 
     public void migrate(Connection connection) throws Exception {
